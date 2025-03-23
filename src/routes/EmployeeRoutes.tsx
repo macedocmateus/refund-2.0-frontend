14ca37b2084c9ad7b092767f844076796513b051
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-22 21:31:17 -0300

    add app layout

--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -3,10 +3,14 @@ import { Routes, Route } from "react-router";
 import { Refund } from "../pages/Refund";
 import { NotFound } from "../pages/NotFound";
 
+import { AppLayout } from "../components/AppLayout";
+
 export function EmployeeRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Refund></Refund>}></Route>
+            <Route path="/" element={<AppLayout></AppLayout>}>
+                <Route path="/" element={<Refund></Refund>}></Route>
+            </Route>
 
             <Route path="*" element={<NotFound></NotFound>}></Route>
         </Routes>
