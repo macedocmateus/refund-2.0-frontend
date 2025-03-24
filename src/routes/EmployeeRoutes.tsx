1eb6b28a2b6c7fad47569c3b31bb0fa7b63be3fe
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-24 16:14:57 -0300

    fixed: bug in applayout into div

--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router";
-import { Refund } from "../pages/Refund";
-import { NotFound } from "../pages/NotFound";
 import { AppLayout } from "../components/AppLayout";
+import { Refund } from "../pages/Refund";
 import { Confirm } from "../pages/Confirm";
+import { NotFound } from "../pages/NotFound";
 
 export function EmployeeRoutes() {
     return (
