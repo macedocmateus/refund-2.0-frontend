23dc8a7833987e5c3bdf158e9f441b35e169ee26
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-25 19:40:56 -0300

    add styles for scroll-bar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -49,7 +49,19 @@ export function Dashboard() {
                 </Button>
             </form>
 
-            <div>
+            <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
+                <RefundItem data={REFUND_EXAMPLE}></RefundItem>
                 <RefundItem data={REFUND_EXAMPLE}></RefundItem>
             </div>
         </div>
