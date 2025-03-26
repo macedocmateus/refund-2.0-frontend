2b54a5e725cb5e7430450c8ab2b4b371443ed59e
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-26 14:38:35 -0300

    finished front-end

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,14 +6,35 @@ import { Loading } from "../components/Loading";
 
 const isLoading = false;
 
+// Em casos onde usuário não fez login
+//const session = undefined;
+
+// Usuário logado com uma role especifica
+const session = {
+    user: {
+        role: "",
+    },
+};
+
 export function Routes() {
+    function Route() {
+        switch (session?.user.role) {
+            case "employee":
+                return <EmployeeRoutes></EmployeeRoutes>;
+            case "manager":
+                return <ManagerRoutes></ManagerRoutes>;
+            default:
+                return <AuthRoutes></AuthRoutes>;
+        }
+    }
+
     if (isLoading) {
         return <Loading></Loading>;
     }
 
     return (
         <BrowserRouter>
-            <AuthRoutes></AuthRoutes>
+            <Route></Route>
         </BrowserRouter>
     );
 }
