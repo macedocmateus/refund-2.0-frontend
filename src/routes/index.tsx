8fac96cfff2167b252ca6cafeaf83288c05e0bf2
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-26 14:18:19 -0300

    add loading component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,18 @@ import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
+import { Loading } from "../components/Loading";
+
+const isLoading = false;
 
 export function Routes() {
+    if (isLoading) {
+        return <Loading></Loading>;
+    }
+
     return (
         <BrowserRouter>
-            <ManagerRoutes></ManagerRoutes>
+            <AuthRoutes></AuthRoutes>
         </BrowserRouter>
     );
 }
