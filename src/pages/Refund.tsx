9ea244ab958a34c9c96c9b10a2c88543104a67cc
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-26 14:05:00 -0300

    add view file button

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router";
+
+import fileSvg from "../assets/file.svg";
+
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
@@ -72,13 +75,24 @@ export function Refund() {
                 ></Input>
             </div>
 
-            <Upload
-                filename={filename && filename.name}
-                disabled={!!params.id}
-                onChange={(e) =>
-                    e.target.files && setFilename(e.target.files[0])
-                }
-            ></Upload>
+            {params.id ? (
+                <a
+                    href="https://github.com"
+                    target="_blank"
+                    className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear"
+                >
+                    <img src={fileSvg} alt="Ícone de arquivo" />
+                    Abrir comprovante
+                </a>
+            ) : (
+                <Upload
+                    filename={filename && filename.name}
+                    disabled={!!params.id}
+                    onChange={(e) =>
+                        e.target.files && setFilename(e.target.files[0])
+                    }
+                ></Upload>
+            )}
 
             <Button type="submit" isLoading={isLoading}>
                 {params.id ? "Voltar" : "Enviar"}
