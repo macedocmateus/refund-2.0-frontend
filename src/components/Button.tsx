be04e73530924d00f45655f18144a643c487edb7
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-26 13:20:40 -0300

    add handlepagination

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ const variants = {
     button: {
         base: "h-12",
         icon: "h-12 w-12",
-        iconSmall: "h-10 w-10",
+        iconSmall: "h-8 w-8",
     },
 };
 
@@ -27,8 +27,9 @@ export function Button({
             disabled={isLoading}
             {...rest}
             className={classMerge([
-                "flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress",
+                "flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50",
                 variants.button[variant],
+                isLoading && "cursor-progress",
                 className,
             ])}
         >
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -21,11 +21,27 @@ const REFUND_EXAMPLE = {
 
 export function Dashboard() {
     const [name, setName] = useState("");
+    const [page, setPage] = useState(1);
+    const [totalOfPage, setTotalOfPage] = useState(10);
 
     function fetchRefounds(e: React.FormEvent) {
         e.preventDefault();
 
         console.log(name);
+    }
+
+    function handlePagination(action: "next" | "previous") {
+        setPage((prevPage) => {
+            if (action === "next" && prevPage < totalOfPage) {
+                return prevPage + 1;
+            }
+
+            if (action === "previous" && prevPage > 1) {
+                return prevPage - 1;
+            }
+
+            return prevPage;
+        });
     }
 
     return (
@@ -68,7 +84,12 @@ export function Dashboard() {
                 <RefundItem data={REFUND_EXAMPLE}></RefundItem>
             </div>
 
-            <Pagination current={1} total={10}></Pagination>
+            <Pagination
+                current={page}
+                total={totalOfPage}
+                onNext={() => handlePagination("next")}
+                onPrevious={() => handlePagination("previous")}
+            ></Pagination>
         </div>
     );
 }
