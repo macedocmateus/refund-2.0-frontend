5acf513d87133c14841809da097cc60e8e8086ab
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-04-03 15:54:28 -0300

    communicating signIn with back-end and creating a session

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,9 @@
 import { useActionState } from "react";
 import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+
+import { api } from "../services/api";
+
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
@@ -18,12 +22,17 @@ export function SignIn() {
                 password: formData.get("password"),
             });
 
-            console.log(data);
+            const response = await api.post("/sessions", data);
+            console.log(response.data);
         } catch (error) {
             console.log(error);
 
             if (error instanceof ZodError) {
                 return { message: error.issues[0].message };
+            }
+
+            if (error instanceof AxiosError) {
+                return { message: error.response?.data.message };
             }
 
             return { message: "Não foi possível entrar!" };
