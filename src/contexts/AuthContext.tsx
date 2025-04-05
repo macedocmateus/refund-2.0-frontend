27538eb67c9cc47f8dc17c1a613a4156734f579c
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-04-04 21:56:56 -0300

    typing session

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { createContext, ReactNode } from "react";
 
-export const AuthContext = createContext({});
+type AuthContext = {
+    session: null | UserAPIResponse;
+};
+
+export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
+    const [session, setSession] = useState<null | UserAPIResponse>(null);
+
     return (
-        <AuthContext.Provider value={{ name: "Mateus" }}>
+        <AuthContext.Provider value={{ session }}>
             {children}
         </AuthContext.Provider>
     );
