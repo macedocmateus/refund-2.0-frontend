0aa737b1a3ba760cd18e459ca3c0ade490123550
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-04-06 18:48:26 -0300

    validating refund page

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router";
+import { data, useNavigate, useParams } from "react-router";
+import { z, ZodError } from "zod";
 
 import fileSvg from "../assets/file.svg";
 
@@ -9,10 +10,20 @@ import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
 import { Button } from "../components/Button";
 
+const refundSchema = z.object({
+    name: z
+        .string()
+        .min(3, { message: "Informe um nome claro para sua solicitação" }),
+    category: z.string().min(1, { message: "Informe a categoria" }),
+    amount: z.coerce
+        .number({ message: "Informe um valor válido" })
+        .positive({ message: "Informe um valor superior a 0" }),
+});
+
 export function Refund() {
-    const [category, setCategory] = useState("teste");
-    const [name, setName] = useState("34");
-    const [amount, setAmount] = useState("transport");
+    const [category, setCategory] = useState("");
+    const [name, setName] = useState("");
+    const [amount, setAmount] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [filename, setFilename] = useState<File | null>(null);
 
@@ -26,7 +37,26 @@ export function Refund() {
             return navigate(-1);
         }
 
-        console.log(name, amount, category, filename);
+        try {
+            setIsLoading(true);
+
+            const data = refundSchema.parse({
+                name,
+                category,
+                amount: amount.replace(",", "."),
+            });
+        } catch (error) {
+            console.log(error);
+
+            if (error instanceof ZodError) {
+                return alert(error.issues[0].message);
+            }
+
+            alert("Não foi possível realizar a solicitação");
+        } finally {
+            setIsLoading(false);
+        }
+
         navigate("/confirm", { state: { fromSubmit: true } });
     }
     return (
